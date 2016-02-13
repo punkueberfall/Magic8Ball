23a0a4e90c87ef9acb86261c8e5534c18662454f
Stefan Zuegg <stefan.zuegg@gmail.com>
2016-02-13 14:57:46 +0100

    names of variables

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -3,21 +3,21 @@ import time
 
 response=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","Q","R","S","T","U"]
 
-def teil1():
-    question=raw_input("Whats on your mind: ")
+def Input():
+    raw_input("Whats on your mind: ")
     print "let me think about your question"
     time.sleep(4)
     print random.choice(response)
-    teil2()
+    Answer()
 
-def teil2():
+def Answer():
     print "do you want to ask me again about anything?"
     userchoice=raw_input("yes or no: ")
     if userchoice =="yes":
-        teil1()
+        Input()
     elif userchoice=="no":
         print "ok, have a nice day"
     else:
         print "error with the input. answer again"
-        teil2()
-teil1()
+        Answer()
+Input()
